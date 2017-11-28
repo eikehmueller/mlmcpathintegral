9c9aa45b4278a7dade05fda332ba7e1482f71e0d
Eike Mueller <e.mueller@bath.ac.uk>
2017-11-28 13:00:45 +0000

    Improved Makefile

---
+++ quantum/src/c/local_macbook.mk
@@ -0,0 +1,5 @@
+# Directory of eigen library
+EIGEN_DIR=/usr/local/include/eigen3/
+
+# Compile in debugging mode (set to True, if required)?
+DEBUG=False
---
+++ quantum/src/c/local_mapc-4024.mk
@@ -0,0 +1,5 @@
+# Directory of eigen library
+EIGEN_DIR=/home/em459/Library/eigen-3.3.4/include/eigen3/
+
+# Compile in debugging mode (set to True, if required)?
+DEBUG=False
