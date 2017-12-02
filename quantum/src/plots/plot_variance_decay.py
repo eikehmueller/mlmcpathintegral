7f247aada2fb686dc3cc2de3dab3c565d08688bd
Eike Mueller <eike.h.mueller@gmail.com>
2017-12-02 18:26:50 +0000

    Added fit to data

--- quantum/src/plots/plot_variance_decay.py
+++ quantum/src/plots/plot_variance_decay.py
@@ -1,6 +1,7 @@
 import sys
 import re
 import subprocess
+import math
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib as mpl
@@ -45,39 +46,53 @@ def run_variance(M_lat):
 def run_all(M_lat_list):
     with open('variance.dat','w') as f:
         for M_lat in M_lat_list:
-            a_lat = T_final/M_lat
             variance = run_variance(M_lat)
-            print >> f, ('%12.8f' % a_lat)+'  '+('%12.8f' % variance)
+            print >> f, ('%12.8f' % M_lat)+'  '+('%12.8f' % variance)
 
 def plot_all():
-    a_lat = []
+    M_lat = []
     variance = []
     with open('variance.dat') as f:
         for line in f.readlines():
-            a_lat_tmp, variance_tmp = line.split()
-            a_lat.append(float(a_lat_tmp))
+            M_lat_tmp, variance_tmp = line.split()
+            M_lat.append(int(float(M_lat_tmp)))
             variance.append(float(variance_tmp))
-    a_lat = np.array(a_lat)
+    M_lat = np.array(M_lat)
     variance = np.array(variance)
     plt.clf()
     ax = plt.gca()
     ax.set_xscale('log')
     ax.set_yscale('log')
-    ax.set_xlabel(r'lattice spacing $a_\ell$')
+    ax.set_xlabel(r'number of time slices $M_\ell$')
     ax.set_ylabel(r'Var($Y_\ell$)')
-    p_var = plt.plot(a_lat,variance,
+    p_var = plt.plot(M_lat,variance,
                      linewidth=2,
                      color='blue',
                      marker='o',
                      markeredgewidth=2,
                      markeredgecolor='blue',
                      markerfacecolor='white')[0]
-    a_ref = a_lat[0]
+    ax.set_xlim(0.5*M_lat[0],2.*M_lat[-1])
+    ax.set_xticks(M_lat)
+    ax.set_xticklabels([str(x) for x in M_lat])
+    M_ref = M_lat[0]
     var_ref = variance[0]
-    p_lin = plt.plot([0.2*a_ref,a_ref],[0.5*0.2*var_ref,0.5*var_ref],
+    rho = 8.
+    p_lin = plt.plot([M_ref,rho*M_ref],[0.5*var_ref,0.5/rho*var_ref],
                      linewidth=2,
                      color='black')[0]
-    plt.legend((p_var,p_lin),('variance','linear decay'),'upper left')
+    # Linear fit to data
+    log_variance = np.log(variance)
+    log_M_lat = np.log(M_lat)
+    a_1,a_0 = np.polyfit(log_M_lat,log_variance,1)
+    M_lat = np.exp(np.arange(math.log(M_lat[0])-0.25,math.log(M_lat[-1])+0.25,0.01))
+    C_deltaV = math.exp(a_0)
+    p_fit = plt.plot(M_lat,C_deltaV*M_lat**a_1,
+                     linewidth=2,
+                     color='red')[0]
+    print 'C_deltaV = ',C_deltaV
+
+    plt.legend((p_var,p_lin,p_fit),('variance','linear decay','fit'),'upper right')
     plt.savefig('variance_decay.pdf',bbox_inches='tight')
 
 if (__name__ == '__main__'):
