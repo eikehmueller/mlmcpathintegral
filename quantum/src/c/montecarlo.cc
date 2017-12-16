f127176b3cecfcf76d4e21552f2ed3ddcd7edc5d
Eike Mueller <eike.h.mueller@gmail.com>
2017-12-16 11:03:19 +0000

    Print out both acceptance and rejection probability

--- quantum/src/c/montecarlo.cc
+++ quantum/src/c/montecarlo.cc
@@ -61,7 +61,8 @@ std::pair<double,double> MonteCarloTwoLevel::evaluate_difference() {
 void MonteCarloTwoLevel::show_stats() {
   if (record_stats) {
     std::cout << "=== Sampling statistics ===" << std::endl;
-    std::cout << "  acceptance probability = " << twolevel_sampler.p_accept() << std::endl;
+    std::cout << "  acceptance probability  p = " << twolevel_sampler.p_accept() << std::endl;
+    std::cout << "  rejection probability 1-p = " << 1.-twolevel_sampler.p_accept() << std::endl;
 
   } else {
     std::cout << "No sampling statistics available." << std::endl;
