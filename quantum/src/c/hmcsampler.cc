b4f9255dbf4c5ce7df4faf1cd22b34952ee50f24
Eike Mueller <eike.h.mueller@gmail.com>
2017-12-17 14:03:13 +0000

    Corrected HMC sampler.
    Use correct sequence of Leapfrog steps and also use the total action in P,X space, H(X,P) = T(P) + S(X) to accept/reject

--- quantum/src/c/hmcsampler.cc
+++ quantum/src/c/hmcsampler.cc
@@ -13,42 +13,57 @@ void HMCSampler::draw(std::vector<Path*> x_path) {
   Path* x_path_trial = new Path(M_lat,T_final);
   // Momentum change from force term
   Path* dp_path = new Path(M_lat,T_final);
+  // Initial kinetic energy
+  double T_kin_cur = 0.0;
   // Draw random momentum from normal distribution
   for (unsigned int j=0;j<M_lat;++j) {
-    p_path_cur->data[j] = normal_dist(engine);
+    double tmp = normal_dist(engine);
+    p_path_cur->data[j] = tmp;
+    T_kin_cur += 0.5*tmp*tmp;
   }
   // STEP 1: Integrate deterministic trajectories with symplectic Euler method
   // Copy current state to trial state
   std::copy(x_path_cur->data,
             x_path_cur->data+M_lat,
             x_path_trial->data);
-  double t = 0.0;
-  // Do initial half-step
-  // Momentum update P -> P - 0.5*dt_{hmc}*dS(X)/dX
-  action.force(x_path_trial,dp_path);
-  for(unsigned int j=0;j<M_lat;++j) {
-    p_path_cur->data[j] -= 0.5*dt_hmc*dp_path->data[j];
-  }
-  t += 0.5*dt_hmc;
-  while(t+0.5*dt_hmc<T_hmc) {
-    // Position update X -> X + dt_{hmc}*P
-    for(unsigned int j=0;j<M_lat;++j) {
-      x_path_trial->data[j] += dt_hmc*p_path_cur->data[j];
+  unsigned int n_hmc_steps = floor(T_hmc/dt_hmc);
+  for(unsigned int k =0;k<=n_hmc_steps;++k) {
+    double dt_p = dt_hmc;
+    double dt_x = dt_hmc;
+    if (k==0) {
+      dt_p = 0.5*dt_hmc;
     }
-    // Momentum update P -> P - dt_{hmc}*dS(X)/dX
+    if (k==n_hmc_steps) {
+      dt_p = 0.5*dt_hmc;
+      dt_x = 0.0;
+    }
+    // Calculate force
     action.force(x_path_trial,dp_path);
     for(unsigned int j=0;j<M_lat;++j) {
-      p_path_cur->data[j] -= dt_hmc*dp_path->data[j];
+      // Momentum update P -> P - 0.5*dt_{hmc}*dS(X)/dX
+      p_path_cur->data[j] -= dt_p*dp_path->data[j];
+      // Position update X -> X + dt_{hmc}*P
+      x_path_trial->data[j] += dt_x*p_path_cur->data[j];
     }
-    t += dt_hmc;
+  }
+  // Calculate kinetic energy from trial state at end of trajectory
+  double T_kin_trial = 0.0;
+  for (unsigned int j=0;j<M_lat;++j) {
+    double tmp = p_path_cur->data[j];
+    T_kin_trial += 0.5*tmp*tmp;
   }
   // STEP 2: Accept-reject step
   bool accept = false;
+  // Change in action S(X)
   double deltaS = action.evaluate(x_path_trial) - action.evaluate(x_path_cur);
-  if (deltaS < 0.0) {
+  // Change in kinetic energy T(P)
+  double deltaT = T_kin_trial - T_kin_cur;
+  // Change in H(X,P) = T(P) + S(X)
+  double deltaH = deltaS + deltaT;
+  if (deltaH < 0.0) {
     accept = true;
   } else {
-    double threshold = exp(-deltaS);
+    double threshold = exp(-deltaH);
     accept = (uniform_dist(engine) < threshold);
   }
   // If accepted, copy state
