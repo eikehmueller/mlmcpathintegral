f59bda9663bc038688bb5764c93a0aff51fda7b7
Eike Mueller <e.mueller@bath.ac.uk>
2018-08-09 15:19:35 +0100

    Removed unnecessary code

--- quantum/src/c/quantityofinterest.cc
+++ quantum/src/c/quantityofinterest.cc
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "quantityofinterest.hh"
 /** @file quantityofinterest.cc
  * @brief Implementation of quantityofinterest.hh
@@ -17,11 +18,10 @@ const double QoIXsquared::evaluate(const Path* x_path) {
 const double QoISusceptibility::evaluate(const Path* x_path) {
   double dx = x_path->data[0]-x_path->data[x_path->M_lat-1];
   double Q = mod_pi(dx);
-  double chi = Q*Q;
   for (unsigned int i=1; i<x_path->M_lat; ++i) {
     dx = x_path->data[i]-x_path->data[i-1];
     Q += mod_pi(dx);    
   }
-  chi = Q*Q;
+  double chi = Q*Q;
   return four_pi2_inv*chi/x_path->T_final;
 }
