a8f09514306bac1027b93bac6b44bf31cf224afd
Eike Mueller <e.mueller@bath.ac.uk>
2018-09-10 16:39:14 +0100

    Update setting of local make options on Bath desktop machine.

--- quantum/src/c/local_mapc-4024.mk
+++ quantum/src/c/local_mapc-4024.mk
@@ -1,8 +1,8 @@
 # Include directory of Gnu Scientific Library (GSL)
-GSL_INCLUDE_DIR=/usr/local/include/
+GSL_INCLUDE_DIR=/home/em459/Library/gsl-2.5/include/gsl
 
 # Lib directory of Gnu Scientific Library (GSL)
-GSL_LIB_DIR=/usr/local/lib/
+GSL_LIB_DIR=/home/em459/Library/gsl-2.5/lib/
 
 # Directory of eigen library
 EIGEN_INCLUDE_DIR=/home/em459/Library/eigen-3.3.4/include/eigen3/
