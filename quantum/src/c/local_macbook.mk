71825fc1e13ef915e609e344c15986aaa8606101
Eike Mueller <eike.h.mueller@gmail.com>
2018-09-11 23:33:28 +0100

    Need different linker flags on different computers

--- quantum/src/c/local_macbook.mk
+++ quantum/src/c/local_macbook.mk
@@ -7,5 +7,8 @@ GSL_LIB_DIR=/usr/local/lib/
 # Directory of eigen library
 EIGEN_INCLUDE_DIR=/usr/local/include/eigen3/
 
+# Additional linker flags
+LOCAL_LFLAGS=
+
 # Compile in debugging mode (set to True, if required)?
 DEBUG=False
--- quantum/src/c/local_mapc-4024.mk
+++ quantum/src/c/local_mapc-4024.mk
@@ -7,5 +7,8 @@ GSL_LIB_DIR=/home/em459/Library/gsl-2.5/lib/
 # Directory of eigen library
 EIGEN_INCLUDE_DIR=/home/em459/Library/eigen-3.3.4/include/eigen3/
 
+# Additional linker flags
+LOCAL_LFLAGS=-static
+
 # Compile in debugging mode (set to True, if required)?
 DEBUG=False
