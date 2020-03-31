5d9e21022350526d8ffa9cbd094e4d2b1cc89ef9
eikehmueller <eike.h.mueller@gmail.com>
2020-03-31 11:04:36 +0200

    Bugfix: remove incorrect definition (xi is passed as a parameter)

--- quantum/src/c/renormalisation.cc
+++ quantum/src/c/renormalisation.cc
@@ -5,7 +5,6 @@
  */
 
 double  RenormalisedRotorParameters::deltaI(const double xi) {
-  double xi = T_final/m0;
   double S_hat2 = Sigma_hat(xi,2);
   double S_hat4 = Sigma_hat(xi,4);
   return 0.5*(1.-2.*xi*S_hat2+0.5*xi*xi*(S_hat4-S_hat2*S_hat2))/(1.-2.*xi*S_hat2+xi*xi*(S_hat4-S_hat2*S_hat2));
