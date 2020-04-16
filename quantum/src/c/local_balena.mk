c72adec7897848b5365dfe971e3920cb877737c0
Eike Mueller <e.mueller@bath.ac.uk>
2020-04-16 10:58:50 +0100

    Added local make configuration for Balena

---
+++ quantum/src/c/local_balena.mk
@@ -0,0 +1,13 @@
+# Include directory of Gnu Scientific Library (GSL)
+GSL_INCLUDE_DIR=/apps/gsl/2.1/include/gsl
+# Lib directory of Gnu Scientific Library (GSL)
+GSL_LIB_DIR=/apps/gsl/2.1/lib/
+
+# Directory of eigen library
+EIGEN_INCLUDE_DIR=/home/n/em459/scratch/library/eigen-3.3.7/include/eigen3/
+
+# Additional linker flags
+LOCAL_LFLAGS=
+
+# Compile in debugging mode (set to True, if required)?
+DEBUG=False
