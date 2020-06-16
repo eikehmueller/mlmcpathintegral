efc9ad9250ee4d4e48c112ef0b65d6f438a2b084
eikehmueller <eike.h.mueller@gmail.com>
2020-06-16 12:57:20 +0200

    Bugfix: quadratic term in expansion was incorrect

--- src/expsin2distribution.cc
+++ src/expsin2distribution.cc
@@ -9,7 +9,7 @@ double ExpSin2Distribution::evaluate(const double x, const double sigma) const {
   double besselI0;
   if (sigma>100.) {
     double sigma_inv = 1./sigma;
-    besselI0 = 2.*sqrt(M_PI*sigma_inv)*(1.+0.25*sigma_inv+9.0/(32.*sigma_inv*sigma_inv));
+    besselI0 = 2.*sqrt(M_PI*sigma_inv)*(1.+0.25*sigma_inv+1.125*sigma_inv*sigma_inv);
   } else {
     besselI0 = 2*M_PI*gsl_sf_bessel_I0_scaled(0.5*sigma);
   }
