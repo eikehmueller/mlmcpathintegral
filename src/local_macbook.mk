5f8a4cad6d007d13cefbe5b89c63d438013c4c52
eikehmueller <eike.h.mueller@gmail.com>
2020-07-29 12:30:12 +0200

    Update paths following home-brew update

--- src/local_macbook.mk
+++ src/local_macbook.mk
@@ -1,11 +1,11 @@
 # Include directory of Gnu Scientific Library (GSL)
-GSL_INCLUDE_DIR=/usr/local/include/
+GSL_INCLUDE_DIR=/usr/local/Cellar/gsl/2.6/include/
 
 # Lib directory of Gnu Scientific Library (GSL)
-GSL_LIB_DIR=/usr/local/lib/
+GSL_LIB_DIR=/usr/local/Cellar/gsl/2.6/lib/
 
 # Directory of eigen library
-EIGEN_INCLUDE_DIR=/usr/local/include/eigen3/
+EIGEN_INCLUDE_DIR=/usr/local/Cellar/eigen/3.3.7/include/eigen3/
 
 # Additional linker flags
 LOCAL_LFLAGS=
