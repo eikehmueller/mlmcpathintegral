1c7c39ccd0a2e78c5cc59dec5a522ad345ae16c9
eikehmueller <eike.h.mueller@gmail.com>
2021-02-26 12:58:41 +0100

    Evaluate distribution in a more robust way that avoid computation of the modified Bessel function directly (instead, only the scaled Bessel function is required)

--- src/distribution/expcosdistribution.cc
+++ src/distribution/expcosdistribution.cc
@@ -7,6 +7,16 @@
 double ExpCosDistribution::evaluate(const double x,
                                     const double x_p,
                                     const double x_m) const {
-    double Z_norm = 2.*M_PI*gsl_sf_bessel_I0(2.*beta*cos(0.5*(x_p-x_m)));
-    return 1./Z_norm*exp(beta*(cos(x-x_p)+cos(x-x_m)));
+    double dx = x_p-x_m;
+    double z = x-x_m;
+    int sign_flip = (dx<0.0)?-1:+1;
+    dx *= sign_flip;
+    if (dx>M_PI) {
+        sign_flip *= -1;
+        dx = 2.*M_PI-dx;
+    }
+    z *= sign_flip;
+    double sigma = 2.*beta*fabs(cos(0.5*dx));
+    double Z_norm = 2.*M_PI*gsl_sf_bessel_I0_scaled(sigma);
+    return 1./Z_norm*exp(sigma*(cos(z-0.5*dx)-1.0));
 }
