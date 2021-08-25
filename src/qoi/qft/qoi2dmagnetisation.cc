0e8f55e26e32721ec90f73cd4c6a5c9c3c663b52
Eike Mueller <e.mueller@bath.ac.uk>
2021-08-25 16:48:55 +0100

    Use correct indexing on rotated lattice

--- src/qoi/qft/qoi2dmagnetisation.cc
+++ src/qoi/qft/qoi2dmagnetisation.cc
@@ -10,12 +10,18 @@ const double QoI2DMagnetisation::evaluate(const std::shared_ptr<SampleState> phi
     for (int i=0;i<Mt_lat;++i) {
         for (int j=0;j<Mx_lat;++j) {
             if ( (not rotated) or ((i+j)%2==0) ) {
-                double theta = phi_state->data[2*lattice->vertex_cart2lin(i,j)];
-                double phi = phi_state->data[2*lattice->vertex_cart2lin(i,j)+1];
+                double theta;
+                double phi;
+                if (rotated) {
+                    theta = phi_state->data[2*lattice->diag_vertex_cart2lin(i,j)];
+                    phi = phi_state->data[2*lattice->diag_vertex_cart2lin(i,j)+1];                    
+                } else {
+                    theta = phi_state->data[2*lattice->vertex_cart2lin(i,j)];
+                    phi = phi_state->data[2*lattice->vertex_cart2lin(i,j)+1];                    
+                }
                 mu[0] += sin(theta)*cos(phi);
                 mu[1] += sin(theta)*sin(phi);
-                mu[2] += cos(theta);
-                
+                mu[2] += cos(theta);                
             }
         }
     }
