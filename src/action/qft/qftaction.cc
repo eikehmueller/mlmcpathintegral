2d153484a9e497f74e9d680cdae0640dd77ce561
Eike Mueller <e.mueller@bath.ac.uk>
2021-09-09 16:08:54 +0100

    include rotation information in info string

--- src/action/qft/qftaction.cc
+++ src/action/qft/qftaction.cc
@@ -7,5 +7,10 @@
 std::string QFTAction::info_string() const {
     std::stringstream sstr;
     sstr << "lattice = " << lattice->getMt_lat() << " x " << lattice->getMx_lat();
+    if (lattice->is_rotated()) {
+        sstr << " [R]";
+    } else {
+        sstr << "    ";
+    }
     return sstr.str();
 }
